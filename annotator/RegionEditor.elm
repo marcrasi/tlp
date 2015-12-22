4d6a386c9bd6aded84f92e6e86e69bbd8ec3d46d
marc <marc@coursera.org>
2015-12-22 17:25:02 -0600

    changing the region editor to use svg

--- annotator/RegionEditor.elm
+++ annotator/RegionEditor.elm
@@ -1,22 +1,30 @@
 module RegionEditor (..) where
 
-import Color exposing (..)
-import Debug
-import Graphics.Collage exposing (..)
-import List exposing (..)
-import Transform2D exposing (..)
+
+import Html
+import List
+import Signal
+import String
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+
+import MouseEvents exposing (..)
+
 
 type State
   = Free
   | Hovering Int
-  | Moving Int
+  | Dragging Int
+
 
 activeIndex : State -> Maybe Int
 activeIndex state =
   case state of
     Free -> Nothing
     Hovering index -> Just index
-    Moving index -> Just index
+    Dragging index -> Just index
+
 
 type alias Model =
   { state : State
@@ -24,121 +32,99 @@ type alias Model =
   , closed : Bool
   }
 
-floatedPoints : Model -> List (Float, Float)
-floatedPoints model =
-  map (\(x, y) -> (toFloat x, toFloat y)) model.points
-
-hoveredIndex : Int -> Int -> Model -> Maybe Int
-hoveredIndex x y model =
-  let
-    fx = toFloat x
-    fy = toFloat y
-    distances = map (\(px, py) -> (fx-px)*(fx-px)+(fy-py)*(fy-py)) (floatedPoints model) 
-    indexedDistances = indexedMap (,) distances
-    eligibleDistances = filter (\(_, d) -> d < 25.0) indexedDistances
-    sortedEligibleDistances = sortBy (\(_, d) -> d) eligibleDistances
-  in
-    head <| map (\(i, _) -> i) sortedEligibleDistances
 
 type Action
-  = MouseDown Int Int
-  | MouseUp Int Int
-  | MouseMove Int Int
+  = ClickPoint Int
+  | ClickCanvas (Int, Int)
+  | EnterPoint Int
+  | ExitPoint Int
+  | MouseDownPoint Int
+  | MouseUpPoint Int
+  | MouseMoveCanvas (Int, Int)
 
-update : Action -> Model -> Model 
+
+update : Action -> Model -> Model
 update action model =
-  model
-    |> updateClicks action
-    |> updateDrags action
-    |> updateHover action
-
-updateClicks : Action -> Model -> Model
-updateClicks action model =
-  case model.state of
-    Free ->
-      case action of
-        MouseUp x y -> { model | points = (x, y) :: model.points, state = Hovering 0 }
-        _ -> model
-    Hovering index ->
-      case action of
-        MouseDown x y -> { model | state = Moving index }
-        _ -> model
-    Moving index ->
-      case action of
-        MouseUp x y -> 
+  case action of
+    ClickPoint index ->
+      if index == List.length model.points - 1
+         then { model | closed = True }
+         else model
+    ClickCanvas (x, y) ->
+      { model | points = (x, y) :: model.points, state = Hovering 0 }
+    EnterPoint index ->
+      { model | state = Hovering index }
+    ExitPoint index ->
+      { model | state = Free }
+    MouseDownPoint index ->
+      { model | state = Dragging index }
+    MouseUpPoint index ->
+      { model | state = Hovering index }
+    MouseMoveCanvas (x, y) ->
+      case model.state of
+        Dragging index ->
           let
-            hmodel = { model | state = Hovering index }
-          in
-            if index == length model.points - 1
-              then { hmodel | closed = True }
-              else hmodel 
-        _ -> model
-
-updateDrags : Action -> Model -> Model
-updateDrags action model =
-  case model.state of
-    Moving index ->
-      case action of
-        MouseMove x y ->
-          let
-            indexedPoints = indexedMap (,) model.points
-            newPoints = map (\(i, p) -> if i == index then (x, y) else p) indexedPoints
+            indexedPoints = List.indexedMap (,) model.points
+            newPoints = List.map (\(i, p) -> if i == index then (x, y) else p) indexedPoints
           in
             { model | points = newPoints }
-        _ -> model
-    _ -> model
+        _ ->
+          model
 
-updateHover : Action -> Model -> Model
-updateHover action model =
-  case model.state of
-    Free ->
-      case action of
-        MouseMove x y ->
-          case hoveredIndex x y model of
-            Just index -> { model | state = Hovering index }
-            Nothing -> { model | state = Free }
-        _ -> model
-    Hovering _ ->
-      case action of
-        MouseMove x y ->
-          case hoveredIndex x y model of
-            Just index -> { model | state = Hovering index }
-            Nothing -> { model | state = Free }
-        _ -> model
-    Moving _ -> model
+addPointToPathString : (Int, Int) -> String -> String
+addPointToPathString (x, y) pathString =
+  let
+    command = if String.length pathString == 0 then "M" else "L"
+  in
+    pathString ++ " " ++ command ++ " " ++ toString x ++ " " ++ toString y
 
-view : Model -> Form
-view model =
-  groupTransform
-    -- hmm, this should not need to know its width and height
-    -- unless I explicitly specify them in some sort of props!
-    -- which might be a good thing to do!
-    (multiply (translation -250 250) (scaleY -1.0))
-    [ outline model
-    , handles model
+
+closePathString : String -> String
+closePathString pathString = pathString ++ " Z"
+
+
+lines : Model -> Svg
+lines model =
+  Svg.path
+    [ d (List.foldl addPointToPathString "" model.points)
+    , fill "none"
+    , stroke "black"
+    , strokeWidth "1"
     ]
+    []
 
-handles : Model -> Form
-handles model =
-  let
-    indexedPoints = indexedMap (,) (floatedPoints model)
-    pointsWithActive = map (\(index, point) -> (point, (activeIndex model.state) == Just index)) indexedPoints 
-  in
-    group <| map (\(point, active) -> handle point active) pointsWithActive 
 
-handle : (Float, Float) -> Bool -> Form
-handle point active =
-  let
-    lineStyle = if active then solid green else solid red
-  in
-    move point <| outlined lineStyle (circle 5.0) 
+point : Signal.Address Action -> Int -> Bool -> (Int, Int) -> Svg 
+point address index active (x, y) = 
+  circle
+    [ cx (toString x)
+    , cy (toString y)
+    , r "5"
+    , fill "black"
+    , stroke (if active then "blue" else "black")
+    , strokeWidth "1"
+    , onClick (always (Signal.message address (ClickPoint index)))
+    , onMouseOver (always (Signal.message address (EnterPoint index)))
+    , onMouseOut (always (Signal.message address (ExitPoint index)))
+    , onMouseDown (always (Signal.message address (MouseDownPoint index)))
+    , onMouseUp (always (Signal.message address (MouseUpPoint index)))
+    ]
+    []
 
-outline : Model -> Form
-outline model =
-  let
-    fps = floatedPoints model
-    maybeClosed = if model.closed
-      then append fps (take 1 fps)
-      else fps
-  in
-    traced (solid red) (path maybeClosed) 
+
+points : Signal.Address Action -> Model -> Svg
+points address model =
+  g
+    []
+    (List.indexedMap (\i p -> point address i (Just i == activeIndex model.state) p) model.points)
+
+
+view : Signal.Address Action -> Model -> Html.Html
+view address model =
+  svg
+    [ width "500"
+    , height "500"
+    , onClick (\v -> Signal.message address (ClickCanvas v))
+    , onMouseMove (\v -> Signal.message address (MouseMoveCanvas v))
+    ]
+    [ lines model, points address model ]
--- annotator/editor.elm
+++ annotator/editor.elm
@@ -1,28 +1,12 @@
-import Graphics.Collage exposing (..)
-import Graphics.Element exposing (..)
-import Mouse
+import Html
 import Signal
 
 import RegionEditor exposing (..)
 
-height : Int
-height = 500
 
-width : Int
-width = 500
+mailbox : Signal.Mailbox Action
+mailbox = Signal.mailbox (MouseMoveCanvas (0, 0))
 
-actions : Signal Action
-actions =
-  let
-    pressChange = Signal.dropRepeats Mouse.isDown
-    mouseDown = Signal.filterMap (\isDown -> if isDown then Just () else Nothing) () pressChange
-    mouseUp = Signal.filterMap (\isDown -> if not isDown then Just () else Nothing) () pressChange
-  in
-    Signal.mergeMany
-      [ Signal.map (\(x, y) -> MouseMove x y) Mouse.position
-      , Signal.map (\(x, y) -> MouseUp x y) (Signal.sampleOn mouseUp Mouse.position)
-      , Signal.map (\(x, y) -> MouseDown x y) (Signal.sampleOn mouseDown Mouse.position)
-      ]
 
 initialModel : Model
 initialModel =
@@ -31,6 +15,7 @@ initialModel =
   , closed = False
   }
 
-main : Signal Element
-main =
-  Signal.map (\model -> collage width height [view model]) (Signal.foldp update initialModel actions) 
+
+main : Signal Html.Html
+main = 
+  Signal.map (view mailbox.address) (Signal.foldp update initialModel mailbox.signal)
