50df45a8d9d1a950fdfed4553e00189ee75e3ad7
marc <marc@coursera.org>
2016-01-30 23:26:04 -0800

    hours resource works

--- server/Handler/Hours.hs
+++ server/Handler/Hours.hs
@@ -2,19 +2,21 @@
 
 module Handler.Hours (getHoursR) where
 
-import Import
+import Import hiding (fromList)
 
 import Database.Persist.TH
 import Data.Text (Text)
 import Database.Persist.Sqlite
 import Data.Conduit
 import qualified Data.Conduit.List as CL
+import Data.Map (fromList)
 
 import Handler.ArgumentParser
+import Handler.ResourceResponse
 
 data Hour = Hour
-  { start :: UTCTime
-  , frameCount :: Int
+  { start :: Int64
+  , frameCount :: Int64
   }
 
 instance ToJSON Hour where
@@ -30,8 +32,15 @@ getHoursR = do
       Nothing -> getAll
       Just otherQuery -> invalidArgs ["Unknown query '" ++ otherQuery ++ "'."]
 
+convertFromPersist [PersistInt64 hour, PersistInt64 count] = Just $ Hour hour count
+convertFromPersist _ = Nothing
+
 getAll :: Handler Value
 getAll = do
-    let sql = "select datetime((strftime('%s', captured_at) / 3600) * 3600, 'unixepoch') interval, count(1) from frame group by interval order by interval"
-    runDB $ rawQuery sql [] $$ CL.mapM_ (liftIO . print)
-    returnJson $ (10 :: Int)
+    let sql = "select (strftime('%s', captured_at) / 3600) * 3600 interval, count(1) from frame group by interval order by interval"
+    hours <- runDB $ rawQuery sql [] $$ CL.map convertFromPersist =$ CL.consume
+    returnJson $ ResourceResponse
+      { elements = hours
+      , linked = fromList []
+      , pagination = Nothing
+      }
